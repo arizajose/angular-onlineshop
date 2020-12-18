97ad60b52cc496ba30a628d600d9aa6d977e00cc
Jose Ariza <arizasalcedoja@gmail.com>
2020-12-17 20:54:20 -0500

    Diseño inicial del componente products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+import { ProductsComponent } from './components/products/products.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
 
 const routes: Routes = [
-  {path:'',component:MainComponent},
+  {path:'', component: MainComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent },
+  {path:'products', component: ProductsComponent},
+  {path:'shoppingcart', component: ShoppingcartComponent}
 ];
 
 @NgModule({
